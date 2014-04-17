787d041fe1c0a9091ca6b334f1216dff7e29cb98
thegame <koumad.salim@gmail.com>
2014-04-17 21:48:39 +0200

    Correction de l'encodage

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -5,7 +5,7 @@ import org.grails.plugins.csv.CSVMapReader
 class BootStrap {
 
     def init = { servletContext ->
-        new File("dataCorpus.csv").toCsvReader(["charset":"UTF-8", "separatorChar":";", "skipLines":1]).eachLine { tokens ->
+        new File("dataCorpus.csv").toCsvReader(["charset":"iso-8859-1", "separatorChar":";", "skipLines":1]).eachLine { tokens ->
             new Livre(titre:tokens[3], nombreExemplaires: 0, nombreExemplairesDisponibles: 0, type:new TypeDocument(intitule: tokens[1]).save(failOnError: true)).save(failOnError: true)
         }
     }
